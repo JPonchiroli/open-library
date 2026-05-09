ddaab8fa836108e609b5ab1dc3f72dc95f6e4251
JPonchiroli <joaopedroponqueroli@gmail.com>
2026-05-09 17:15:05 -0300

    refact: altera layout de pagina

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -42,36 +42,58 @@ export default function Dashboard({ books, booksCount }: DashboardProps) {
 
                     {books.slice(0,3).map(book => (
 
-                        <div className="relative p-4 aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                            <img src={book.book_cover_url} alt="capaLivro" />
-                            <h2>{book.title}</h2>
-                            <p>{book.author}</p>
-                            <p>
-                                {book.available_copies >= 1
-                                    ? "Disponível"
-                                    : "Sem estoque"}
-                            </p>
+                        <div className="relative flex gap-4 p-4 overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
 
-                            <div className='flex justify-center'>
+                            <div className="shrink-0">
+                                <img
+                                    className="w-32 h-44 object-cover rounded-lg"
+                                    src={book.book_cover_url}
+                                    alt="capaLivro"
+                                />
+                            </div>
 
-                                <button
-                                    disabled={book.available_copies < 1}
-                                    className={`
-                                        p-3 rounded-2xl border border-solid
-                                        transition
-                                        ${
-                                            book.available_copies >= 1
-                                                ? 'bg-black text-white border-black hover:cursor-pointer hover:bg-white hover:text-black dark:bg-white                  dark:text-black dark:border-white dark:hover:bg-black dark:hover:text-white'
-                                                : 'border-gray-500 text-gray-500 cursor-not-allowed opacity-50'
-                                        }
-                                    `}
-                                >
-                                    {book.available_copies >= 1
-                                        ? 'Solicitar Empréstimo'
-                                        : 'Sem Estoque'}
-                                </button>
+                            <div className="flex flex-1 flex-col justify-between">
+
+                                <div className="space-y-2">
+                                    <h2 className="text-xl font-bold">
+                                        {book.title}
+                                    </h2>
+
+                                    <p className="text-sm opacity-80">
+                                        {book.author}
+                                    </p>
+
+                                    <p>
+                                        {book.available_copies >= 1
+                                            ? "Disponível"
+                                            : "Sem estoque"}
+                                    </p>
+                                </div>
+
+                                <div className="flex justify-end">
+
+                                    <button
+                                        disabled={book.available_copies < 1}
+                                        className={`
+                                            p-3 rounded-2xl border border-solid
+                                            transition
+
+                                            ${
+                                                book.available_copies >= 1
+                                                    ? 'bg-black text-white border-black hover:cursor-pointer hover:bg-white hover:text-black dark:bg-white dark:text-black dark:border-white dark:hover:bg-black dark:hover:text-white'
+                                                    : 'border-gray-500 text-gray-500 cursor-not-allowed opacity-50'
+                                            }
+                                        `}
+                                    >
+                                        {book.available_copies >= 1
+                                            ? 'Solicitar Empréstimo'
+                                            : 'Sem Estoque'}
+                                    </button>
+
+                                </div>
 
                             </div>
+
                         </div>
 
                     ))}
