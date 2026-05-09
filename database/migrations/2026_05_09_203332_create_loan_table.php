af3361861098140a4a8f4e75b77446e0a2a0e19f
JPonchiroli <joaopedroponqueroli@gmail.com>
2026-05-09 17:34:56 -0300

    feat: cria migration para tabela de emprestimos

---
+++ database/migrations/2026_05_09_203332_create_loan_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loan', function (Blueprint $table) {
+            $table->id('loan_id');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->date('loan_date');
+            $table->date('return_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loan');
+    }
+};
