30bc0658b775a6ac0300482dded10699bd624298
JPonchiroli <joaopedroponqueroli@gmail.com>
2026-05-09 21:13:42 -0300

    feat: adiciona controller para retornar dados para web

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,18 +3,29 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
-use App\Models\Books;
+use App\Models\Book;
+use App\Models\Loan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $books = Books::all();
-        $booksCount = Books::where('available_copies', '>', 0)->count();
+        $books = Book::all();
+        $booksCount = Book::where('available_copies', '>', 0)->count();
+        $loansCount = Loan::where('user_id', '=', Auth::id())->count();
+
+        $loans = DB::select('SELECT bk.title, bk.author, ln.loan_date, ln.return_date
+                            FROM loans ln
+                            LEFT JOIN books bk on (bk.id = ln.book_id)
+                            WHERE ln.user_id = ?', [Auth::id()]);
 
         return Inertia::render('dashboard', [
             'books' => $books,
-            'booksCount' => $booksCount
+            'booksCount' => $booksCount,
+            'loans' => $loans,
+            'loansCount' => $loansCount
         ]);
     }
 }
